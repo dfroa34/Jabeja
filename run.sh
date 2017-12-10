bcf2efc461ede3e98c426651c24bc7f20df50faf
Diego <o90G03e1258145>
2017-12-10 20:06:24 +0100

    First commit

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -Xmx5000m -jar target/assignment4-jabeja-1.0-jar-with-dependencies.jar $@
+read -p "Press [Enter] key to start backup..."
---
+++ src/main/java/se/kth/jabeja/Main.java
@@ -0,0 +1,79 @@
+package se.kth.jabeja;
+
+import org.apache.log4j.Logger;
+import se.kth.jabeja.io.CLI;
+import se.kth.jabeja.io.GraphReader;
+import se.kth.jabeja.config.Config;
+import se.kth.jabeja.rand.RandNoGenerator;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+
+
+public class Main {
+    final static Logger logger = Logger.getLogger(Main.class);
+
+    /**
+     * Holds all the configurations parameters.
+     */
+    private Config config;
+
+    HashMap<Integer, Node> graph;
+
+    public static void main(String[] args) throws IOException {
+        new Main().startApp(args);
+    }
+
+    private void startApp(String[] args) throws IOException {
+        config = (new CLI()).parseArgs(args);
+
+
+
+        //set seed for the application
+        //Note for the results to be deterministic use
+        //only one random generator.
+        RandNoGenerator.setSeed(config.getSeed());
+
+        //read the input graph
+        HashMap<Integer, Node> graph = readGraph();
+
+        //start JaBeJa
+        startJabeja(graph);
+    }
+
+    /**
+     * parses the input graph
+     *
+     * @return
+     */
+    private HashMap<Integer, Node> readGraph() {
+        GraphReader graphReader = new GraphReader();
+        graph = graphReader.readGraph(config.getGraphFilePath(), config.getGraphInitialColorPolicy(), config.getNumPartitions());
+        return graph;
+    }
+
+    /**
+     * start the jabeja algorithm
+     *
+     * @param graph
+     * @return
+     */
+    private void startJabeja(HashMap<Integer, Node> graph) throws IOException {
+       
+        if(config.getTask() == 1)
+        {
+         logger.info("Running task");
+         Jabeja host = new Jabeja(graph, config);
+           host.startJabeja();
+        }
+        else if(config.getTask() == 2)
+        {
+          logger.info("Running task2");
+          Jabeja_task2 host_2 = new Jabeja_task2(graph, config);
+            host_2.startJabeja();
+        }
+        
+      
+    }
+}
